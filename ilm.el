4c267a53966d975df58e89283e63e75aa274cfa6
Ildenir Barbosa <ildenir+github@googlemail.com>
2020-08-02 12:26:19 -0300

    Adiciona algum keybinding xahlee

--- ilm.el
+++ ilm.el
@@ -141,6 +141,10 @@
 (define-key c-mode-map (kbd "C-c o") 'ff-find-other-file)
 (define-key c++-mode-map (kbd "C-c o") 'ff-find-other-file)
 
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-2") 'split-window-vertically)
+(global-set-key (kbd "M-3") 'split-window-horizontally)
+(global-set-key (kbd "M-0") 'delete-window)
 
 (require 'switch-window)
 (global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
