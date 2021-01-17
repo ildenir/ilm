c27ff0f0c2346f84bfe8bc5f27016030f02072e8
Ildenir Barbosa <ildenir+github@googlemail.com>
2021-01-17 10:48:32 -0300

    Add Copying

--- ilm-misc.el
+++ ilm-misc.el
@@ -1,4 +1,28 @@
 ;;; ilm-misc.el --- Rotinas comuns
+;; Copyright (C) 2017, 2021 Ildenir Barbosa
+
+;; Author: Ildenir Barbosa <il.denir@gmail.com>
+;; Created: dom 17 jan 2021 10:47:42 -03
+
+;; Version: 0.1
+;; Package-Requires: ((cl-lib "0.5"))
+;; Keywords: script, convenience
+;; URL: http://github.com/ildenir/ilm
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
